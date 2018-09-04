9678da3d570979f3af4802d884486a665ecbb156
LvisRoot <claus.smitt@gmail.com>
2018-09-04 08:41:42 -0300

    first commit

---
+++ toyBA/Debug/src/main.d
@@ -0,0 +1,339 @@
+src/main.o: ../src/main.cpp /home/l_vis/Programas/Eigen_3.3.5/Eigen/Core \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/DisableStupidWarnings.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Macros.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/MKL_support.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Constants.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Meta.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/ForwardDeclarations.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/StaticAssert.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/XprHelper.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Memory.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NumTraits.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MathFunctions.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GenericPacketMath.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MathFunctionsImpl.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/Default/ConjHelper.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/PacketMath.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/MathFunctions.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/Complex.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/TypeCasting.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/Half.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/PacketMathHalf.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/TypeCasting.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/Default/Settings.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/TernaryFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/BinaryFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/UnaryFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/NullaryFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/StlFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/AssignmentFunctors.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/Complex.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/IO.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseCoeffsBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/BlockMethods.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MatrixBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/CommonCwiseUnaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/CommonCwiseBinaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/MatrixCwiseUnaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/MatrixCwiseBinaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/EigenBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Product.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CoreEvaluators.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/AssignEvaluator.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Assign.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ArrayBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/ArrayCwiseUnaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/ArrayCwiseBinaryOps.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/BlasUtil.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseStorage.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NestByValue.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ReturnByValue.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NoAlias.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/PlainObjectBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Matrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Array.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseTernaryOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseBinaryOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseUnaryOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseNullaryOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseUnaryView.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SelfCwiseBinaryOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Dot.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/StableNorm.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Stride.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MapBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Map.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Ref.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Block.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/VectorBlock.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Transpose.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DiagonalMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Diagonal.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DiagonalProduct.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Redux.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Visitor.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Fuzzy.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Swap.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CommaInitializer.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GeneralProduct.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Solve.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Inverse.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SolverBase.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/PermutationMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Transpositions.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/TriangularMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SelfAdjointView.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralBlockPanelKernel.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/Parallelizer.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ProductEvaluators.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixVector.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SolveTriangular.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixMatrixTriangular.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointMatrixVector.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointMatrixMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointProduct.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointRank2Update.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularMatrixVector.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularMatrixMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularSolverMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularSolverVector.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/BandMatrix.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CoreIterators.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ConditionEstimator.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/BooleanRedux.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Select.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/VectorwiseOp.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Random.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Replicate.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Reverse.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ArrayWrapper.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GlobalFunctions.h \
+ /home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/ReenableStupidWarnings.h
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/Core:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/DisableStupidWarnings.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Macros.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/MKL_support.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Constants.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Meta.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/ForwardDeclarations.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/StaticAssert.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/XprHelper.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/Memory.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NumTraits.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MathFunctions.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GenericPacketMath.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MathFunctionsImpl.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/Default/ConjHelper.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/PacketMath.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/MathFunctions.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/Complex.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/SSE/TypeCasting.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/Half.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/PacketMathHalf.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/TypeCasting.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/Default/Settings.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/TernaryFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/BinaryFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/UnaryFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/NullaryFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/StlFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/functors/AssignmentFunctors.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/arch/CUDA/Complex.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/IO.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseCoeffsBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/BlockMethods.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MatrixBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/CommonCwiseUnaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/CommonCwiseBinaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/MatrixCwiseUnaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/MatrixCwiseBinaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/EigenBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Product.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CoreEvaluators.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/AssignEvaluator.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Assign.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ArrayBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/ArrayCwiseUnaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/../plugins/ArrayCwiseBinaryOps.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/BlasUtil.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DenseStorage.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NestByValue.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ReturnByValue.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/NoAlias.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/PlainObjectBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Matrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Array.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseTernaryOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseBinaryOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseUnaryOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseNullaryOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CwiseUnaryView.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SelfCwiseBinaryOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Dot.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/StableNorm.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Stride.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/MapBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Map.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Ref.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Block.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/VectorBlock.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Transpose.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DiagonalMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Diagonal.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/DiagonalProduct.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Redux.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Visitor.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Fuzzy.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Swap.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CommaInitializer.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GeneralProduct.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Solve.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Inverse.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SolverBase.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/PermutationMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Transpositions.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/TriangularMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SelfAdjointView.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralBlockPanelKernel.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/Parallelizer.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ProductEvaluators.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixVector.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/SolveTriangular.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/GeneralMatrixMatrixTriangular.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointMatrixVector.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointMatrixMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointProduct.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/SelfadjointRank2Update.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularMatrixVector.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularMatrixMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularSolverMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/products/TriangularSolverVector.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/BandMatrix.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/CoreIterators.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ConditionEstimator.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/BooleanRedux.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Select.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/VectorwiseOp.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Random.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Replicate.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/Reverse.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/ArrayWrapper.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/GlobalFunctions.h:
+
+/home/l_vis/Programas/Eigen_3.3.5/Eigen/src/Core/util/ReenableStupidWarnings.h:
