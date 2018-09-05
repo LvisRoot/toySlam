eefe73a6eb2c123eaf27bcb078c8b1a0fd35c2cf
LvisRoot <claus.smitt@gmail.com>
2018-09-05 09:05:21 -0300

    Rearranged repo, ready for abs. prior

--- toyBA/src/main.cpp
+++ toyBA/src/main.cpp
@@ -7,15 +7,21 @@
 
 #include <vector>
 #include <iostream>
+#include <random>
+
+#include <chrono>
 
 #include "Eigen/Dense"
 
 
-const static double E_TOL = 0;
-const static int N_IT_MAX = 100;
+const static double E_TOL = 1e-5;
+const static int N_IT_MAX = 1000;
 
 const static double CAM_FX = 1;
 const static int CAM_CX = 0;
+
+const static double CI_COV = 0.5;
+
 
 
 using namespace Eigen;
@@ -30,40 +36,15 @@ typedef struct frame frame;
 
 Vector2d computePointJack(const Vector2d & t_point, const Vector2d & t_pose){
 
-	double dx = 1 /(t_point[1] - t_pose[1]);
-	double dz = CAM_FX * (t_point[0] - t_pose[0]) * dx * dx;
+	double dx = t_point[0] - t_pose[0];
+	double dz = t_point[1] - t_pose[1];
 
-	dx *= CAM_FX;
-
-	return Vector2d(-dx,dz);
+	return Vector2d(-CAM_FX / dz, CAM_FX * dx / (dz * dz));
 }
 
 Vector2d computePoseJack(const Vector2d & t_point, const Vector2d & t_pose){
 
-	double dx = 1 /(t_point[1] - t_pose[1]);
-	double dz = CAM_FX * (t_point[0] - t_pose[0]) * dx * dx;
-
-	dx *= CAM_FX;
-
-	return Vector2d(dx,-dz);;
-}
-
-MatrixXd computeJack(const std::vector<Vector2d> & t_points, const std::vector<frame> & t_frames){
-
-	int nP = t_points.size();
-	int mT = t_frames.size();
-
-	MatrixXd jack(nP*mT,2*(nP+mT));
-
-	for(int j = 0; j < mT ; ++j){
-		for(int i = 0; i < nP ; ++i){
-
-			jack.block<1,2>(nP*j+i,2*j) = computePoseJack(t_points[i],t_frames[j]._pose).transpose();
-
-			jack.block<1,2>(nP*j+i,2*mT+2*i) = computePointJack(t_points[i],t_frames[j]._pose).transpose();
-		}
-	}
-	return jack;
+	return -computePointJack(t_point,t_pose);
 }
 
 MatrixXd computeJack(const VectorXd & t_state, const int t_nP, const int t_mT){
@@ -90,22 +71,6 @@ double computeErep(const double & t_q, const Vector2d & t_p, const Vector2d & t_
 	return t_q - project(t_p,t_t);
 }
 
-VectorXd computeF(const std::vector<Vector2d> & t_points, const std::vector<frame> & t_frames){
-
-	int nP = t_points.size();
-	int mT = t_frames.size();
-
-	VectorXd eRep(nP*mT);
-
-	for(int j = 0; j < mT ; ++j){
-		for(int i = 0; i < nP ; ++i){
-
-			eRep[nP*j+i] = computeErep(t_frames[j]._meas[i],t_points[i],t_frames[j]._pose);
-		}
-	}
-	return eRep;
-}
-
 VectorXd computeF(const VectorXd & t_meas, const VectorXd & t_state, const int t_nP, const int t_mT){
 
 	VectorXd eRep(t_nP*t_mT);
@@ -121,10 +86,24 @@ VectorXd computeF(const VectorXd & t_meas, const VectorXd & t_state, const int t
 
 double gnOpt(std::vector<Vector2d> & t_points, std::vector<frame> & t_frames, int t_nIt = N_IT_MAX){
 
+	const double 	m_xp = 0,
+					m_zp = 0,
+					m_xt = 0,
+					m_zt = 0;
+	const double 	cov_xp = CI_COV,
+					cov_zp = CI_COV,
+					cov_xt = CI_COV,
+					cov_zt = CI_COV;
+	std::default_random_engine gen;
+	std::normal_distribution<double> 	xpDist(m_xp, cov_xp),
+										zpDist(m_zp, cov_zp),
+										xtDist(m_xt, cov_xt),
+										ztDist(m_zt, cov_zt);
+
 	int nP = t_points.size();
 	int mT = t_frames.size();
 
-	double l_LM = 1e-2;
+	double l_LM = 1e-5;
 
 	if(t_nIt > N_IT_MAX)
 		t_nIt = N_IT_MAX;
@@ -134,11 +113,11 @@ double gnOpt(std::vector<Vector2d> & t_points, std::vector<frame> & t_frames, in
 
 
 	for (int j = 0; j < mT; ++j) {
-		state.segment(2*j,2) = t_frames[j]._pose;
+		state.segment<2>(2*j) = t_frames[j]._pose + Vector2d(xtDist(gen),ztDist(gen));
 	}
 
-	for (int i = 2*mT; i < nP+mT; ++i) {
-		state.segment(2*i,2) = t_points[i];
+	for (int i = 0; i < nP; ++i) {
+		state.segment<2>(2*(mT+i)) = t_points[i] + Vector2d(xpDist(gen),zpDist(gen));
 	}
 
 	for(int j = 0; j < mT ; ++j){
@@ -148,93 +127,86 @@ double gnOpt(std::vector<Vector2d> & t_points, std::vector<frame> & t_frames, in
 		}
 	}
 
+	std::cout << "\n State CI: " << state.transpose();
+
 	//VectorXd fErrOld(nP*mT);
 
-	for (int i = 0; i < t_nIt; ++i) {
+	double eNorm = std::numeric_limits<double>::max();
+
+	for (int i = 0; i < t_nIt && eNorm >= E_TOL; ++i) {
 
 		VectorXd fErr = computeF(meas,state,nP,mT);
 		MatrixXd jack = computeJack(state,nP,mT);
-		MatrixXd hess = jack.transpose()*jack;
+		MatrixXd hess = jack.transpose()*jack + l_LM * MatrixXd::Identity(jack.cols(),jack.cols());
 
-		std::cout << "\nJacobian Mat: \n" << jack<< '\n';
-		std::cout << "\nHessian Mat: \n" << hess << '\n';
+		state += hess.ldlt().solve(-jack.transpose()*fErr);
 
-		state += (hess + l_LM * MatrixXd::Identity(jack.cols(),jack.cols())).inverse() * (-jack.transpose()*fErr);
-
-		std::cout << "\n Opt it " << i << " repError vector: \n" << fErr.norm();
+		eNorm = fErr.norm();
 	}
 
-	return 0;
+	for (int j = 0; j < mT; ++j)
+		 t_frames[j]._pose = state.segment<2>(2 * j);
+
+	for (int i = 0; i < nP; ++i)
+		t_points[i] = state.segment<2>(2 * (mT + i));
+
+	return eNorm;
 }
+
 
 int main(int argc, char **argv) {
 
 	int nP = atoi(argv[1]);
 	int mT = atoi(argv[2]);
+	int nIts = atoi(argv[3]);
 
 	std::vector<Vector2d> points, poses;
 	std::vector<frame> frames;
 
-//	for (int i = 0; i < nP; ++i) {
-//		points.push_back(Vector2d::Random());
-//	}
-//
-//	for (int j = 0; j < mT; ++j) {
-//
-//		Vector2d pose = Vector2d::Random();
-//		std::vector<double> meas;
-//
-//		for (int i = 0; i < nP; ++i)
-//			meas.push_back(project(points[i],pose));
-//
-//		frames.push_back(frame{pose,meas});
-//	}
+    const double 	m_xp = 4,
+					m_zp = 3,
+    				m_xt = 3,
+					m_zt = -2;
+    const double 	cov_xp = 2,
+					cov_zp = 0.1,
+					cov_xt = 3,
+					cov_zt = 0.1;
+    std::default_random_engine gen;
+    std::normal_distribution<double> 	xpDist(m_xp, cov_xp),
+										zpDist(m_zp, cov_zp),
+										xtDist(m_xt, cov_xt),
+										ztDist(m_zt, cov_zt);
 
-	nP = 4;
-	mT = 4;
-
-	points.push_back(Vector2d(1,1));
-	points.push_back(Vector2d(3,1));
-	points.push_back(Vector2d(5,2));
-	points.push_back(Vector2d(8,3));
-
-	poses.push_back(Vector2d(2,-1));
-	poses.push_back(Vector2d(4,-1));
-	poses.push_back(Vector2d(6,-2));
-	poses.push_back(Vector2d(7,-3));
+	for (int i = 0; i < nP; ++i) {
+		points.push_back(Vector2d(xpDist(gen),zpDist(gen)));
+	}
 
 	for (int j = 0; j < mT; ++j) {
 
+		Vector2d pose = Vector2d(xtDist(gen),ztDist(gen));
 		std::vector<double> meas;
 
-		for (int i = 0; i < nP; ++i){
-			meas.push_back(project(points[i],poses[j]));
+		for (int i = 0; i < nP; ++i)
+			meas.push_back(project(points[i],pose));
 
-			std::cout << "\n Point: " << i << " Proj. in Pose: " << j << '\n' << project(points[i],poses[j]) << '\n';
-		}
-		frames.push_back(frame{poses[j],meas});
+		frames.push_back(frame{pose,meas});
 	}
 
-	//MatrixXd jack = computeJack(points,frames);
+	std::vector<Vector2d> pGT = points;
+	std::vector<frame> fGT = frames;
 
-	//std::cout << "Jacobian Mat: \n" << jack << '\n';//computePoseJack(points[0],poses[0]);
-	//std::cout << "JacobianT Mat: \n" << jack.transpose() << '\n';//computePoseJack(points[0],poses[0]);
+	gnOpt(points,frames,nIts);
 
-	//std::cout << "\nHessian Mat: \n" << jack.transpose() * jack << '\n';
+	std::cout << "\n Poses: \n";
+	for (int i = 0; i < mT; ++i){
+		std::cout << fGT[i]._pose.transpose() << '\n';
+		std::cout << frames[i]._pose.transpose() << "\n\n";
+	}
+	std::cout << "\n Points: \n";
+	for (int i = 0; i < nP; ++i){
+		std::cout << pGT[i].transpose() << '\n';
+		std::cout << points[i].transpose() << "\n\n";
+	}
 
-	//Map<Matrix<double,2*points.size(),1>> state (points[0].data(),2);
-
-//	for (int i = 0; i < points.size(); ++i) {
-//		std::cout << points[i] << '\n';
-//	}
-//
-//	Map<MatrixXd> state (points[0].data(),points.size()*2,1);
-//
-//	std::cout << "\n Mapped Points: \n" << state << '\n';
-
-	//gnOpt(points,frames,1);
 
 }
-
-
-
